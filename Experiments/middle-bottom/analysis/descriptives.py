8323b0951f6f22e0d8e61864b69ffc5ab3aadd0b
nolan <nolanbconaway@gmail.com>
2017-01-24 21:34:57 -0600

    drafted copytweak and hs

--- Experiments/middle-bottom/analysis/descriptives.py
+++ Experiments/middle-bottom/analysis/descriptives.py
@@ -1,14 +1,16 @@
 import sqlite3, sys
 import pandas as pd
+import numpy as np
 
 sys.path.insert(0, "../../../Modules/") # generate-categories/Modules
 import utils
 
-pd.set_option('display.width', 200, 'precision', 2)
+pd.set_option('display.width', 120, 'precision', 2)
 
 con = sqlite3.connect('../data/experiment.db')
 participants = pd.read_sql_query("SELECT * from participants", con)
 counterbalance = pd.read_sql_query("SELECT * from counterbalance", con)
+stats = pd.read_sql_query("SELECT * from betastats", con)
 con.close()
 
 print participants.shape
@@ -24,3 +26,23 @@ print pd.pivot_table(
 	index = 'condition',
 	aggfunc = 'size'
 	)
+
+
+
+stats = pd.merge(stats, participants, on = 'participant')
+from scipy.stats import ttest_ind, mannwhitneyu
+cols = ['area','between','within',
+				'correlation', 
+				'drange', 'xrange', 'yrange', 'xstd', 'ystd']
+
+for i in cols:
+	gs = list(stats.groupby('condition')[i])
+	d = dict(gs)
+	ms = dict([(k, np.mean(v)) for k,v in d.items()])
+	p = mannwhitneyu(d['Middle'], d['Bottom']).pvalue
+
+	S = i
+	for k,v in d.items():
+		S += '\t' + k + ' = ' + str(round(np.mean(v),3))
+	S += '\t' + 'p = ' + str(round(p,3))
+	print S
