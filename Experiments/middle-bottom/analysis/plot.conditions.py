67ad025c56fadfa1e2d7ffd739c9b9a8f4efe840
nolan <nolanbconaway@gmail.com>
2017-03-19 19:57:16 -0500

    update prior to joes lookover

--- Experiments/middle-bottom/analysis/plot.conditions.py
+++ Experiments/middle-bottom/analysis/plot.conditions.py
@@ -16,16 +16,17 @@ con.close()
 
 
 
-f, ax = plt.subplots(1, 2, figsize=(3, 1.5))
+f, ax = plt.subplots(1, 2, figsize=(3.3, 1.5))
 for i, k  in enumerate(list(alphas)):
 	h = ax[i]
 	funcs.plotclasses(h, stimuli, alphas[k], [])		
 
-	h.axis([-1.2, 1.2, -1.2, 1.2])
+	h.axis(np.array([-1, 1, -1, 1])*1.25)
 	# h.text(-1.1, 1.1, k, ha = 'left', va = 'top')
+	h.set_title(k)
 	[i.set_linewidth(0.5) for i in h.spines.itervalues()]
 
 f.savefig('conditions.pdf', bbox_inches='tight', transparent=False)
 
-# path = '../../../Manuscripts/cogsci-2017/figs/middle-bottom-conditions.pgf'
-# funcs.save_as_pgf(f, path)
+path = '../../../Manuscripts/cog-psych/figs/e2-conditions.pgf'
+funcs.save_as_pgf(f, path)
