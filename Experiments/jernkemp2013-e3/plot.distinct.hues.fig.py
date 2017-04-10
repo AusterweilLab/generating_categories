038097b842452c3f0e3295c74138a57595a2f877
nolan <nolanbconaway@gmail.com>
2017-04-10 15:11:35 -0400

    added stats to jk13 analysis, adjusted binning

--- Experiments/jernkemp2013-e3/plot.distinct.hues.fig.py
+++ Experiments/jernkemp2013-e3/plot.distinct.hues.fig.py
@@ -6,13 +6,11 @@ import seaborn as sns
 sns.set_style("whitegrid")
 sns.set_palette("cubehelix")
 
-
-from scipy.stats import binom
+from scipy.stats import binom, chisquare
 
 execfile('Imports.py')
 import Modules.Funcs as funcs
 from JK13 import JK13, JKFuncs
-
 
 # import data
 con = sqlite3.connect('experiment.db')
@@ -21,13 +19,19 @@ training = pd.read_sql_query("SELECT * from training", con)
 generation = pd.read_sql_query("SELECT * from generation", con)
 con.close()
 
+# set hue categories
+# hues = np.array([[0,30,60,90,120,150,180,210,240,270,300,330,360]]).T / 360.0
+hues = np.array([[0, 45, 90, 135, 180, 225, 270, 315, 360]]).T / 360.0
+
+for i in range(1,len(hues)):
+	print hues[i-1][0], (hues[i][0] + hues[i-1][0])/2.0, hues[i][0]
 
 # get number of distinct items for each participant
 shared_hues = dict( (i,np.array([])) for i in JK13.conditions)
 for (i, c), rows in generation.groupby(['participant','condition']):
 	idx = (training.participant == i) & (training.condition == c)
-	As = JKFuncs.dummycode_colors(training.loc[idx].Hue.as_matrix())
-	Bs = JKFuncs.dummycode_colors(rows.Hue.as_matrix())
+	As = JKFuncs.dummycode_colors(training.loc[idx].Hue.as_matrix(), hues=hues)
+	Bs = JKFuncs.dummycode_colors(rows.Hue.as_matrix(), hues=hues)
 	num_distinct = np.sum(np.in1d(Bs,As)==1)
 	shared_hues[c] = np.append(shared_hues[c],num_distinct)
 
@@ -40,23 +44,25 @@ linestyles = dict(
 
 fh = plt.figure(figsize = (4.5,3))
 
+# compute expected values
+n = 6 # number of trials
+p = 2.0/(len(hues)-1) # baserate of overlap (2 categories, N bins)
+expected = binom.pmf(range(n+1), n, p) * 22
+
 x = range(0,7)
 for i, k in enumerate(['Positive','Neutral','Negative']):
 	v = shared_hues[k]
 	y = funcs.histvec(v, x, density = False)
+	print k, chisquare(y, expected)
 	plt.plot(x, y, linestyles[k], lw = 1, label = k)
 
 # set binomial line
-n = 6 # number of trials
-p = 2.0/6.0 # baserate of overlap (2 categories, 6 bins)
-expected = binom.pmf(range(n+1), n, p) * 22
-# plt.plot(x, expected, '--', color = 'k', lw = 1, label = 'Expected (Binomial)')
-plt.bar(x, expected + 0.75, 1, 
+plt.bar(x, expected + 0.75, 1, # added 0.75 to adjust for bottom of bar at -0.75
 	facecolor = [0.7,0.7,0.7], edgecolor = 'k', linewidth = 0.75,
 	label = 'Expected (Binomial)', bottom = -0.75)
 
 
-plt.axis([-0.6,6.6,-0.75,22.5])
+plt.axis([-0.6,6.6,-0.75,22.75])
 plt.gca().xaxis.grid(False)
 plt.xticks(range(7))
 plt.legend(frameon = True, edgecolor = 'k')
