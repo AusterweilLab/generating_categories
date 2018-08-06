2cd1aebaf87f96f7509280f415e4f1aca59c924c
shixianliew <shixianliew@gmail.com>
2018-08-06 14:23:40 -0500

    Fixing some changes. Sorry for vague statements but this should be quick.

--- Modules/Classes/__init__.py
+++ Modules/Classes/__init__.py
@@ -1,3 +1,3 @@
 import Simulation
-from Packer import Packer, CopyTweak, CopyTweakRep
+from Packer import Packer, CopyTweak, CopyTweakRep,PackerRep
 from ConjugateJK13 import ConjugateJK13, RepresentJK13
