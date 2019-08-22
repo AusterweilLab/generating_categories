7de5eb749db0393b443dc6ae7a1925b5a17b3bbf
shixianliew <shixianliew@gmail.com>
2019-08-22 09:25:35 -0500

    Preparing for merge back to master.

--- Experiments/cogpsych-code/ImportModels.py
+++ Experiments/cogpsych-code/ImportModels.py
@@ -4,6 +4,7 @@ from Modules.Classes import Simulation
 from Modules.Classes import CopyTweak
 from Modules.Classes import CopyTweakRep
 from Modules.Classes import Packer
+from Modules.Classes import PackerEuc
 from Modules.Classes import PackerRep
 from Modules.Classes import ConjugateJK13
 from Modules.Classes import RepresentJK13
